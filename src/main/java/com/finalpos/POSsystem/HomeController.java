effa0f54f47e33fc18192fe37be07cbd278422d5
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-11-13 21:46:24 +0700

    new configs

---
+++ src/main/java/com/finalpos/POSsystem/HomeController.java
@@ -0,0 +1,14 @@
+package com.finalpos.POSsystem;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/finalpos/POSsystem/PoSsystemApplication.java
@@ -0,0 +1,13 @@
+package com.finalpos.POSsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoSsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoSsystemApplication.class, args);
+	}
+
+}
