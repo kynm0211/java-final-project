2228d4f67463fbd33b0d7ed46b5eb594bce02734
Minh Kỳ <521h0093@student.tdtu.edu.vn>
2023-11-19 20:52:11 +0700

    update

--- src/main/java/com/finalpos/POSsystem/Controller/AccountController.java
+++ src/main/java/com/finalpos/POSsystem/Controller/AccountController.java
@@ -22,11 +22,53 @@ public class AccountController {
     @PostMapping("/register_admin")
 
     public Package register_admin(@RequestBody UserModel model) {
-        System.out.println(model);
-        model.setRole("administator");
-        model.setImage(defaultAvatar);
-        model.setCreated_at(java.time.LocalDateTime.now());
-        db.save(model);
-        return new Package(0, "success", model);
+        try{
+            System.out.println(model);
+            model.setRole("administator");
+            model.setImage(defaultAvatar);
+            model.setCreated_at(java.time.LocalDateTime.now());
+            db.save(model);
+            return new Package(0, "success", model);
+        }
+        catch (Exception e){
+            return new Package(404, e.getMessage(), null);
+        }
     }
+
+    @PostMapping("/login")
+    public Package login(){
+        try{
+            return new Package(0, "success", null);
+        }catch (Exception e){
+            return new Package(404, e.getMessage(), null);
+        }
+    }
+
+    @PostMapping("/change-password")
+    public Package changePassword(){
+        try{
+            return new Package(0, "success", null);
+        }catch (Exception e){
+            return new Package(404, e.getMessage(), null);
+        }
+    }
+
+    @GetMapping("/profile")
+    public Package profile(){
+        try{
+            return new Package(0, "success", null);
+        }catch (Exception e){
+            return new Package(404, e.getMessage(), null);
+        }
+    }
+
+    @PostMapping("/direct")
+    public Package direct(){
+        try{
+            return new Package(0, "success", null);
+        }catch (Exception e){
+            return new Package(404, e.getMessage(), null);
+        }
+    }
+
 }
